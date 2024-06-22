9687b96ce0b4a9b7b6fffba33d4a473ed930149e
Fahad <fahadashfaq996@gmail.com>
2024-06-23 03:06:14 +0500

    scheduler trial

---
+++ myapp/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import {BryntumScheduler} from "@bryntum/scheduler-react"
+import React from 'react';
+
+function App() {
+  const [crudManagerConfig] = React.useState({
+    loadUrl: "data.json",
+    autoLoad: true,
+  })
+
+  const [columnsConfig] = React.useState([
+    {text: "Name", field:"name", width: 130},
+    {text: "Age", field: "age", width: 50}
+  ])
+
+  const [eventEditConfig] = React.useState({
+    items: {
+      nameField: {
+        label: "Title",
+      },
+      textField:{
+        label: "Text",
+        type: "text",
+        name: "text"
+      }
+    }
+  })
+
+  const handleBeforeEventEditSHow = React.useCallback(({editor, eventRecord}) => {
+    const textField = editor.widgetMap.textField;
+    textField.hidden = eventRecord.name === "Assign Document";
+  }, [])
+
+  return (
+    <>
+    <BryntumScheduler
+    viewPreset = "hourAndDay"
+    startDate= {new Date(2024, 2, 20, 6)}
+    endDate= {new Date(2024, 2, 24, 20)}
+    crudManager={crudManagerConfig}
+    columns={columnsConfig}
+    eventEditFeature = {eventEditConfig}
+    onBeforeEventEditShow={handleBeforeEventEditSHow}
+    />
+    </>
+  );
+}
+
+export default App;
